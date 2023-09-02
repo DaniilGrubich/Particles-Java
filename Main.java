5eac0982d2c7407182ed70728735c5f126f05ecb
danpgru <grubich01@gmail.com>
2023-09-02 00:41:28 -0400

    update

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a test.");
+        System.out.println("This is a test.");
 
     }
 }
